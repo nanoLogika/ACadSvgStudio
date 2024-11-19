5d00c3a980891036652c008019404d2c19f863b2
Nihad Karajko <nihad.karajko@wdw-elab.de>
2024-11-19 15:11:01 +0100

    UpdateSvgViewer on Load

--- ACadSvgStudio/MainForm.cs
+++ ACadSvgStudio/MainForm.cs
@@ -86,6 +86,8 @@ namespace ACadSvgStudio {
 
             recentlyOpenedFilesManager = new RecentlyOpenedFilesManager();
             updateRecentlyOpenedFiles();
+
+            UpdateSvgViewer();
         }
 
 
