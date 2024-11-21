bd32eaa0cd265ff0c310f77b94011f3d7a1de534
Nihad Karajko <nihad.karajko@wdw-elab.de>
2024-11-21 10:23:04 +0100

    Add Copyright Notice to SvgViewerUserControl

--- ACadSvgStudio/SvgViewerUserControl.cs
+++ ACadSvgStudio/SvgViewerUserControl.cs
@@ -1,4 +1,11 @@
-﻿using Svg;
+﻿#region copyright LGPL nanoLogika
+//  Copyright 2023, nanoLogika GmbH.
+//  All rights reserved. 
+//  This source code is licensed under the "LGPL v3 or any later version" license. 
+//  See LICENSE file in the project root for full license information.
+#endregion
+
+using Svg;
 
 namespace ACadSvgStudio {
 
